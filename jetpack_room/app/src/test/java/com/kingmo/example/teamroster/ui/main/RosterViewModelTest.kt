e072cc0d4361e5f2ed349fdfb71bd2cd9a59a8c6
Maurice Smith <mo.maurice@gmail.com>
2019-11-21 21:56:46 -0500

    adding unit tests for RosterViewModel

--- jetpack_room/app/src/test/java/com/kingmo/example/teamroster/ui/main/RosterViewModelTest.kt
+++ jetpack_room/app/src/test/java/com/kingmo/example/teamroster/ui/main/RosterViewModelTest.kt
@@ -1,10 +1,16 @@
 package com.kingmo.example.teamroster.ui.main
 
+import android.view.View
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.kingmo.example.teamroster.database.Player
 import com.kingmo.example.teamroster.database.PlayerDao
 import com.kingmo.example.teamroster.utils.schedulers.TestSchedulerProvider
+import com.kingmo.example.teamroster.viewmodels.ErrorViewModel
+import com.kingmo.example.teamroster.viewmodels.PlayerViewModel
 import com.kingmo.example.teamroster.viewmodels.RosterViewModel
+import io.reactivex.Observable
 import org.junit.After
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 
@@ -27,7 +33,6 @@ class RosterViewModelTest {
 
     @Before
     fun setUp() {
-        //`when`(playerDao.findPlayerById(any())).thenReturn()
         viewModel = RosterViewModel(playerDao, TestSchedulerProvider())
     }
 
@@ -36,8 +41,50 @@ class RosterViewModelTest {
     }
 
     @Test
-    fun shouldInvokeGetPlayers() {
+    fun shouldLoadPlayersSuccessWithPlayers() {
+        `when`(playerDao.getPlayers()).thenReturn(Observable.just(listOf(Player(playerId = 123, firstName = "Paul", lastName = "Wall"))))
+
         viewModel.loadPlayers()
+
+        val playerList: List<PlayerViewModel>? = viewModel.playersLiveData.value as List<PlayerViewModel>
+
+        assertEquals(View.GONE, viewModel.noPlayersFoundVisibility.value)
+        assertEquals(View.VISIBLE, viewModel.playerRosterVisibility.value)
+
+        assertTrue(playerList!!.isNotEmpty())
+        assertEquals(123, playerList.get(0).getPlayerId())
+        assertEquals("Paul", playerList.get(0).getFirstName())
+        assertEquals("Wall", playerList.get(0).getLastName())
+
+        verify(playerDao).getPlayers()
+    }
+
+    @Test
+    fun shouldLoadPlayersSuccessWithoutPlayers() {
+        `when`(playerDao.getPlayers()).thenReturn(Observable.just(emptyList()))
+
+        viewModel.loadPlayers()
+
+        assertNull(viewModel.playersLiveData.value)
+        assertEquals(View.VISIBLE, viewModel.noPlayersFoundVisibility.value)
+        assertEquals(View.GONE, viewModel.playerRosterVisibility.value)
+
+        verify(playerDao).getPlayers()
+    }
+
+    @Test
+    fun shouldNotLoadPlayersWhenErrorOccurs() {
+        `when`(playerDao.getPlayers()).thenReturn(Observable.error(Exception("Error Message.")))
+
+        viewModel.loadPlayers()
+
+        assertNull(viewModel.playersLiveData.value)
+        assertEquals(View.GONE, viewModel.noPlayersFoundVisibility.value)
+        assertEquals(View.GONE, viewModel.playerRosterVisibility.value)
+
+        val errorViewModel: ErrorViewModel? = viewModel.errorViewModel.value
+        assertEquals("Error Message.", errorViewModel?.message)
+        assertEquals(View.VISIBLE, errorViewModel?.errorVisibility)
 
         verify(playerDao).getPlayers()
     }
