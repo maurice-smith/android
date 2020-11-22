3b8249330108e67a026898f303debab842e5f36c
Maurice Smith <mo.maurice@gmail.com>
2020-11-21 23:27:20 -0500

    setting jetpack navigation deps

---
+++ jetpack_room/app/src/main/java/com/kingmo/example/teamroster/view/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.kingmo.example.teamroster.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kingmo.example.teamroster.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
