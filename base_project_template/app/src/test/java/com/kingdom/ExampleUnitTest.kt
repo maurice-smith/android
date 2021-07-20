2cd45b3d6b2783dc2b028b4b776f0d1262fbee0d
Maurice Smith <mo.maurice@gmail.com>
2021-07-19 21:34:36 -0400

    Setting up Jetpack Navigation, data/view Binding, Coroutine with coroutines, and deps.

--- base_project_template/app/src/test/java/com/kingdom/ExampleUnitTest.kt
+++ base_project_template/app/src/test/java/com/kingdom/ExampleUnitTest.kt
@@ -1,17 +1,29 @@
 package com.kingdom
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runBlockingTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
 import org.junit.Test
+import testutil.CoroutineTestRule
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@ExperimentalCoroutinesApi
 class ExampleUnitTest {
+    @get:Rule
+    val coroutineTestRule = CoroutineTestRule()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun flowTestExample() = coroutineTestRule.dispatcher.runBlockingTest {
+        val testFlow = flowOf(listOf("Mike", "Paul"))
+
+        assertThat(testFlow.first()[0]).isEqualTo("Mike")
     }
 }
